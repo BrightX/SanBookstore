e5bfa8a63d0845ceeb1bdafb55fd50d74c049577
徐亮亮 <1626769688@qq.com>
2020-12-14 11:31:05 +0800

    创建 Spring Boot 项目

---
+++ src/main/java/com/bright/bookstore/SanBookstoreApplication.java
@@ -0,0 +1,16 @@
+package com.bright.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 徐亮亮
+ */
+@SpringBootApplication
+public class SanBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bright/bookstore/SanBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bright.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
