f91e4daccf0e9a3591f4429f3af4d135571b77d9
徐亮亮 <1626769688@qq.com>
2020-12-14 12:04:11 +0800

    添加商铺管理控制层

---
+++ src/main/java/com/bright/bookstore/controller/ShopController.java
@@ -0,0 +1,47 @@
+package com.bright.bookstore.controller;
+
+import com.bright.bookstore.pojo.Shop;
+import com.bright.bookstore.pojo.user.AuthUser;
+import com.bright.bookstore.service.ShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author 徐亮亮
+ * @since 2020/12/2
+ */
+@Controller
+public class ShopController {
+
+    @Autowired
+    private ShopService shopService;
+
+    @GetMapping("/shopCart")
+    public String shopCart() {
+        return "shop/shopCart";
+    }
+
+    @GetMapping("/manageShop")
+    public String manageShop() {
+        return "shop/manageShop";
+    }
+
+    @PostMapping("/applyShop")
+    @ResponseBody
+    public Shop applyShop(HttpSession session, String name, String sendAddress) {
+        AuthUser user = (AuthUser) session.getAttribute("user");
+        Shop shop = new Shop();
+        shop.setName(name);
+        shop.setSendAddress(sendAddress);
+        shop.setUsername(user.getUsername());
+        shop.setUserId(user.getId());
+        shopService.applyShop(user, shop);
+        session.setAttribute("user", user);
+        return shop;
+    }
+}
